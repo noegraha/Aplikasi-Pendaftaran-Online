b50577580fb6515c81a6dcdd75f90291f6631f3d
noegraha <okeloh09>
2019-06-25 11:50:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,10 @@ if ( isset($_SESSION['user_login']) && $_SESSION['user_login'] != '' ) {
     $halaman = $_SESSION['user_login'];
 
     header('location:on-'. $halaman);
-    exit();
+    
 } else {
     header('location:login.php');
     exit();
+    echo "test2";
+    echo "test"
 }
